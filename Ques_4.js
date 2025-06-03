ddd7c9e66db81a93a92aa0a84bbb782fc9dfd577
vaishnavi-successiveTech <vaishnavi.gupta@successive.tech>
2025-06-03 16:59:01 +0530

    Delete Ques_4.js

--- Ques_4.js
+++
@@ -1,8 +0,0 @@
-//Write a function expression that takes in a number and returns its square.
-
-const square = function multiply(num){
-               return num*num;
-}
-
-console.log(square(4));
-console.log(square(8));
