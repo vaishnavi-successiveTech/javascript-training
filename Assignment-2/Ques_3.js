e426eebac909969b20a0fb000258587c47377b73
Vaishnavi Gupta <vaishnavi.gupta@vaishnaviss473.successivetech.com>
2025-06-04 18:30:55 +0530

    feat:Added all questions of assignment-3

--- Assignment-2/Ques_3.js
+++ Assignment-2/Ques_3.js
@@ -4,7 +4,7 @@ const add = function(num,num2){
     return num+num2;
 }
 // function call as an argument
-const subtract = function (add,num5)
+const subtract = function(add,num5)
 {
     result = num5 - add(3, 4);
     return result;
