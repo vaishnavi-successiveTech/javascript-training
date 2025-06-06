3f3393288381f2fb5fde035d9f7317d533e11a4a
Vaishnavi Gupta <vaishnavi.gupta@vaishnaviss473.successivetech.com>
2025-06-06 10:13:30 +0530

    assignment-2

---
+++ Assignment-2/Ques_6.js
@@ -0,0 +1,10 @@
+// Write a function expression that takes in a number and returns true if it's even and false if it's odd.
+const evenOdd= function(num){
+    if(num%2==0)
+        return true;
+    else
+        return false;
+}
+
+console.log(evenOdd(10));
+console.log(evenOdd(9));
