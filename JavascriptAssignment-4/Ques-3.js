625ac4346e6ed1f06774f1a16f475e35d68ffab7
Vaishnavi Gupta <vaishnavi.gupta@vaishnaviss473.successivetech.com>
2025-06-09 13:16:29 +0530

    Ques-3 fix

--- JavascriptAssignment-4/Ques-3.js
+++ JavascriptAssignment-4/Ques-3.js
@@ -1,5 +1,5 @@
 // 3. Write a program to give count of variables in a string ("abcabcdabbcc" => "a3b4c4d1")
-function CountOccurence(string) {
+function countOccurence(string) {
   let str=string.toLowerCase();
   let mapAlphabets = new Map(); // Map declare
   for (let i = 0; i < str.length; i++) {
@@ -17,4 +17,4 @@ function CountOccurence(string) {
 }
 
 const str = "abcabcdabbcc";
-console.log(CountOccurence(str));
+console.log(countOccurence(str));
