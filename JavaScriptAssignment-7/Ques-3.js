3929871cc4edfbffae1cde900cdb3a59b61f6edd
Vaishnavi Gupta <vaishnavi.gupta@vaishnaviss473.successivetech.com>
2025-06-12 16:47:34 +0530

    Do changes in format

--- JavaScriptAssignment-7/Ques-3.js
+++ JavaScriptAssignment-7/Ques-3.js
@@ -5,12 +5,25 @@
 //   }, 1000);
 // }
 
-setTimeout(()=>{
-let promise = new Promise((resolve, reject) => {
-  fetch(`https://api.thecatapi.com/v1/images/search?limit=10`)
-    .then((response) => response.json().then((data) => resolve(data)))
-    .catch((err) => reject(err));
-});
-promise.then(data=>console.log(data)).catch(msg=>console.log(msg));
-},);
+
+function callAPI(){
+    let promise = new Promise((resolve, reject) => {
+        let success=true;
+        setTimeout(()=>{
+        if(success){
+
+             fetch(`https://api.thecatapi.com/v1/images/search?limit=10`)
+             .then((response)=>response.json()
+             .then((data)=>resolve(data)))
+             .catch((err)=>reject(err));
+        }
+    },4000);
+
+    });
+    const result= promise.then(data=>console.log(data)).catch(msg=>console.log(msg));
+    return result;
+}
+callAPI()
+
+
 
--- JavaScriptAssignment-7/Ques-8.js
+++ JavaScriptAssignment-7/Ques-8.js
@@ -38,3 +38,4 @@ async function operationFetchData(){
 
 
 operationFetchData();
+
